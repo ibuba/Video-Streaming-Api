05e3fd1eebb185ae02fda806951881bd5060490d
ibuba <ibesto682@gmail.com>
2023-03-21 19:23:42 -0500

    email senderService added

--- src/main/java/miu/videokabbee/service/emailSender/EmailService.java
+++ src/main/java/miu/videokabbee/service/emailSender/EmailService.java
@@ -6,14 +6,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.stereotype.Service;
 
 import java.util.Properties;
-//import javax.mail.Authenticator;
-//import javax.mail.Message;
-//import javax.mail.MessagingException;
-//import javax.mail.PasswordAuthentication;
-//import javax.mail.Session;
-//import javax.mail.Transport;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
+
 @Service
 public class EmailService {
 
